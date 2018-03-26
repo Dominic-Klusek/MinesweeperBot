f69702ea51b60c3f370547094ef40facad7fd09f
Goldensonic96 <Goldensonic96@yahoo.com>
2018-03-26 00:17:48 -0400

    Initial Commit

---
+++ AIMinesweeperBot/MinesweeperBot.py
@@ -0,0 +1,18 @@
+class MineSweeperBot:
+    def __init__(self):
+        '''
+        Initialize the bots parameters with some sort of data
+        '''
+        pass
+    def performmove(self, x, y):
+        '''
+        Method to have bot perform a move, will need x and y coordinates
+        '''
+        pass
+    def thinkofmove(self):
+        '''
+        Bot will analyze board and decide what is the best move
+        (aka what move doesn't hit a mine)
+        '''
+        pass
+    def 
